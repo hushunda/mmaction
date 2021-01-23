8d3b7fd715f4af3435320548a3e808527c6deaf8
zjrobot_hsd1 <hushunda@zhejianglab.com>
2021-01-23 17:58:45 +0800

    添加了res101和sk101模型

---
+++ data_tools/find_no_finish_video.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+找到没有生成完光流的video
+'''
+
+import os
+
+data_root = '../data/ucf101/rawframes'
+out_txt = './'
+
+nofinish_video = []
+for cls in os.listdir(data_root):
+    cls_path = os.path.join(data_root,cls)
+    for video_name in os.listdir(cls_path):
+        all_data = os.listdir(os.path.join(cls_path,video_name))
+        nun_frames = len([x for x in all_data if x.startswith('img')])
+        num_flow_x = len([x for x in all_data if x.startswith('flow_x')])
+        num_flow_y = len([x for x in all_data if x.startswith('flow_y')])
+        if num_flow_x==num_flow_y and num_flow_x+1==nun_frames and num_flow_x>0:
+            continue
+        nofinish_video.append(os.path.join(cls,video_name))
+
+print('num of no finish video:  ',len(nofinish_video))
+with open(out_txt) as f:
+    for line in nofinish_video:
+        f.writelines(line+'\n')
--- mmaction/models/tenons/backbones/__init__.py
+++ mmaction/models/tenons/backbones/__init__.py
@@ -1,12 +1,14 @@
 from .bninception import BNInception
 from .resnet import ResNet
 
+from .sknet import SKnet101
 from .inception_v1_i3d import InceptionV1_I3D
 from .resnet_i3d import ResNet_I3D
 from .resnet_s3d import ResNet_S3D
 from .resnet_i3d_slowfast import ResNet_I3D_SlowFast
 from .resnet_r3d import ResNet_R3D
 from .c3d import C3D
+from .resnet101 import ResNet101
 
 __all__ = [
     'BNInception',
@@ -16,5 +18,10 @@ __all__ = [
     'ResNet_S3D',
     'ResNet_I3D_SlowFast',
     'ResNet_R3D',
-    'C3D'
+    'SKnet101',
+    'C3D',
+    'ResNet101',
+
 ]
+
+
