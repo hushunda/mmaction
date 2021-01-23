e3e99d0ed74e16e90b59aa178fee3403f954ab3a
zjrobot_hsd1 <hushunda@zhejianglab.com>
2021-01-23 20:18:06 +0800

    修改训练配置参数

--- tools/ucf101/train_ucf101_rgb_inception.sh
+++ tools/ucf101/train_ucf101_rgb_inception.sh
@@ -1,3 +1,3 @@
-cd ../..
+cd ../../
 
-./tools/dist_train_recognizer.sh configs/TSN/ucf101/tsn_rgb_bninception.py 1 --validate --work_dir ./workdir/ucf101_rgb_inception
+./tools/dist_train_recognizer.sh configs/TSN/ucf101/tsn_rgb_bninception.py 6 --validate
