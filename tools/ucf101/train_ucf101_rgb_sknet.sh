706c726a7219f97aafc5822c32e27dc298b93986
zjrobot_hsd1 <hushunda@zhejianglab.com>
2021-01-24 13:14:17 +0800

    修改运行文件

--- tools/ucf101/train_ucf101_rgb_sknet.sh
+++ tools/ucf101/train_ucf101_rgb_sknet.sh
@@ -1,7 +1,4 @@
 cd ../../
 
-<<<<<<< HEAD
 ./tools/dist_train_recognizer.sh configs/TSN/ucf101/tsn_rgb_sknet.py 6 --validate
-=======
-./tools/dist_train_recognizer.sh configs/TSN/ucf101/tsn_rgb_sknet.py 6 --validate
->>>>>>> 1537a8b5c52b762cb8241092243f82d927eee13a
+
