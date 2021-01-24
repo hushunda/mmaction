6fb5754d28333684836332b4c860c5e62a5d1576
hushunda <hushunda@zjlab.com>
2021-01-24 12:56:24 +0800

    1

--- tools/dist_test_recognizer.sh
+++ tools/dist_test_recognizer.sh
@@ -2,4 +2,4 @@
 
 PYTHON=${PYTHON:-"python"}
 
-$PYTHON -m torch.distributed.launch --nproc_per_node=$3 $(dirname "$0")/test_recognizer.py $1 $2 --launcher pytorch ${@:4}
+$PYTHON -m torch.distributed.launch --nproc_per_node=$3 $(dirname "$0")/test_recognizer.py $1 $2 --launcher pytorch $(@:4)
