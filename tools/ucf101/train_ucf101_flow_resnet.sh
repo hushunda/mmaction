461f5a6d83077965dbc920092228ea0417698070
hushunda <hushunda@zjlab.com>
2021-01-25 10:03:43 +0800

    训练设置修改

--- tools/ucf101/train_ucf101_flow_resnet.sh
+++ tools/ucf101/train_ucf101_flow_resnet.sh
@@ -1,3 +1,3 @@
 cd ../../
 
-./tools/dist_train_recognizer.sh configs/TSN/my_data/tsn_flow_resnet.py 6 --validate
+./tools/dist_train_recognizer.sh configs/TSN/ucf101/tsn_flow_resnet101.py 6 --validate
